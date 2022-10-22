8e029d332afdecd4b665fb06c48830934c643b9c
Alin-Gabriel Drăghici <guky667@gmail.com>
2022-10-22 20:50:39 +0300

    Update XHRs.py

--- XHRs.py
+++ XHRs.py
@@ -8,39 +8,47 @@ async def xhrGetHandler(endpoint, headers):
     try:
         async with ClientSession() as session:
             async with session.get(yarl.URL(endpoint, encoded = True), headers = headers) as response:
-                print(response.status)
-                response = await response.read()
-                response = json.loads(response)
-                print (response)
+                try:
+                    response = json.loads(response)                    
+                except:
+                    response = json.loads(response.content.decode('utf-8'))
+                finally:
+                    return response
     
     except Exception as err:
-        print('Unhandled exception for URL: ',err)
+        print('Unhandled exception for URL: ', endpoint, err)
 
 async def xhrPutHandler(endpoint, payload, headers):
     """# Callback handler for put XHR"""
     try:
         async with ClientSession() as session:
             async with session.put(yarl.URL(endpoint, encoded = True), data = payload, headers = headers) as response:
-                print(response.status)
-                response = await response.read()
-                response = json.loads(response)
-                print (response)
+                print('Status code: ', response.status)
+                try:
+                    response = json.loads(response)                    
+                except:
+                    response = json.loads(response.content.decode('utf-8'))
+                finally:
+                    return response
     
     except Exception as err:
-        print('Unhandled exception for URL: ',err)
+        print('Unhandled exception for URL: ', endpoint, err)
 
 async def xhrPostHandler(endpoint, payload, headers):
     """# Callback handler for post XHR"""
     try:
         async with ClientSession() as session:
             async with session.post(yarl.URL(endpoint,encoded = True), data = payload, headers = headers) as response:
-                print(response.status)
                 response = await response.read()
-                response = json.loads(response)
-                print (response)
-    
+                try:
+                    response = json.loads(response)                    
+                except:
+                    response = json.loads(response.content.decode('utf-8'))
+                finally:
+                    return response
+
     except Exception as err:
-        print('Unhandled exception for URL: ',err)
+        print('Unhandled exception for URL: ', endpoint, err)
 
 
 def get(endpoint, headers = None):
@@ -52,10 +60,15 @@ def get(endpoint, headers = None):
     if isinstance(endpoint,str):
         tasks = [asyncio.ensure_future(xhrGetHandler(endpoint, headers))]
         loop.run_until_complete(asyncio.wait(tasks))
+        return tasks[0]._result
 
     if isinstance(endpoint,list):
         tasks = [asyncio.ensure_future(xhrGetHandler(url, headers)) for url in endpoint]
         loop.run_until_complete(asyncio.wait(tasks))
+        responses = {}
+        for i in range(len(endpoint)):
+            responses[endpoint[i]] = tasks[i]._result
+        return responses
 
 def put(endpoint, payload, headers = None):
     """# PUT XHR
@@ -64,8 +77,9 @@ def put(endpoint, payload, headers = None):
     """
     tasks = [asyncio.ensure_future(xhrPutHandler(endpoint, payload, headers))]
     loop.run_until_complete(asyncio.wait(tasks))
+    return tasks[0]._result
 
-def post(endpoint, payload, headers):
+def post(endpoint, payload, headers = None):
     """# POST XHR
     Takes a single URL and the payload to post
     Can take headers dict as the 3rd param
@@ -73,3 +87,4 @@ def post(endpoint, payload, headers):
 
     tasks = [asyncio.ensure_future(xhrPostHandler(endpoint, payload, headers))]
     loop.run_until_complete(asyncio.wait(tasks))
+    return tasks[0]._result
